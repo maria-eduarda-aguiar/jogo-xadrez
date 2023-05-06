34bc964b33335bdccc6fb916653d310474959a7a
Maria Eduarda <maria.eduarda.aguiar1395@gmail.com>
2023-05-06 20:55:06 -0300

    Criado a classe ExcecaoTabuleiro e feito o tratamento de erros

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -9,39 +9,62 @@ public class Tabuleiro {
 	
 	// Criação do construtor
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new ExcecaoTabuleiro("Erro ao criar o tabuleiro. É necessário que tenha pelo menos uma linha e uma coluna.");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
 	}
 	
-	// Criação dos getters e setters
+	// Criação dos getters
 	public int getLinhas() {
 		return linhas;
-	}
-
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
 	}
 
 	public int getColunas() {
 		return colunas;
 	}
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
 	
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExiste(linha, coluna)) {
+			throw new ExcecaoTabuleiro("Posicão não existe no tabuleiro.");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	// Método para colocar peças em uma posição no tabuleiro
 	public void colocarPeca(Peca peca, Posicao posicao) {
+		if (existePeca(posicao)) {
+			throw new ExcecaoTabuleiro("Já existe uma peça na posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
+	
+	// Método auxiliar para verificar se a posição existe
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	// Método para verificar se a posição existe
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	// Método para verificar se existe peça na posição
+	public boolean existePeca(Posicao posicao) {
+		if (!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Posição não existe no tabuleiro.");
+		}
+		return peca(posicao) != null;
+	}
+	
 }
