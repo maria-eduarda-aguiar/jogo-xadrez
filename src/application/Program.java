cfa3e78687a3f4d2514d15c6e8e618b6118e37a7
Maria Eduarda <maria.eduarda.aguiar1395@gmail.com>
2023-05-06 14:03:54 -0300

    Primeira classe criada: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-
-		System.out.println("Jogo de Xadrez");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
