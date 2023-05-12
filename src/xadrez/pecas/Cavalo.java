020a8b86257281517cf979d35ae4f49fa38836de
MargioryS <margiorysimas@hotmail.com>
2023-05-12 14:07:40 -0300

    Ajuste na classe da peça Cavalo

--- src/xadrez/pecas/Cavalo.java
+++ src/xadrez/pecas/Cavalo.java
@@ -29,49 +29,49 @@ public class Cavalo extends PecaXadrez{
 		
 		Posicao p = new Posicao(0, 0);
 		
-		// 
+		// Verificar acima do Cavalo
 		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 2);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// 
+		// Verificar abaixo do Cavalo
 		p.setValores(posicao.getLinha() - 2, posicao.getColuna() - 1);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// 
+		// Verificar à esquerda do Cavalo
 		p.setValores(posicao.getLinha() - 2, posicao.getColuna() + 1);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// 
+		// Verificar à direita do Cavalo
 		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 2);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// Verificar a diagonal noroeste do Rei
+		// Verificar a diagonal noroeste do Cavalo
 		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 2);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// 
+		// Verificar a diagonal nordeste do Cavalo 
 		p.setValores(posicao.getLinha() + 2, posicao.getColuna() + 1);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// 
+		// Verificar a diagonal sudoeste do Cavalo
 		p.setValores(posicao.getLinha() + 2, posicao.getColuna() - 1);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		// 
+		// Verificar a diagonal sudeste do Cavalo
 		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 2);
 		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
